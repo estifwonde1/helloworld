2ccd24c6899d6ecdd6c6d34c046657e3087e389e
estifwonde <estifanoswondwossen396@gmail.com>
2025-04-23 15:51:29 +0300

    intial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginHtml from"eslint-plugin-html";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx,html}"], plugins: { js, html:pluginHtml }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx,html}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+  {plugins:{html:pluginHtml}},
+  {rules:{semi : ["error", "always"]}},
+  {settings: { react: { version: "detect" } }},
+]);
