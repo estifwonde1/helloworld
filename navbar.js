9075f50547a3d3649bc854bfba86d936e631534b
estifwonde <estifanoswondwossen396@gmail.com>
2025-04-25 20:03:56 +0300

    Fix Eslint errors

--- navbar.js
+++ navbar.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamMenu = document.querySelector('.ham-menu');
-    const offscreenMenu = document.querySelector('.off-screen-menu');
+  const hamMenu = document.querySelector('.ham-menu');
+  const offscreenMenu = document.querySelector('.off-screen-menu');
 
-    if (hamMenu && offscreenMenu) {
-        hamMenu.addEventListener('click', () => {
-            hamMenu.classList.toggle('active'); // Toggles the active class on the menu
-            offscreenMenu.classList.toggle('active'); // Toggles the active class on the off-screen menu
+  if (hamMenu && offscreenMenu) {
+     hamMenu.addEventListener('click', () => {
+        hamMenu.classList.toggle('active'); 
+        offscreenMenu.classList.toggle('active'); 
         });
-    } else {
+  } else {
         console.error('Ham menu or off-screen menu not found.');
-    }
+  }
 });
