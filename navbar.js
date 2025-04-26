96e77fec636e109e0fd28b9a2d4432bad0274ef4
estifwonde <estifanoswondwossen396@gmail.com>
2025-04-26 07:41:55 +0300

    Fix linter trial 11

--- navbar.js
+++ navbar.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamMenu = document.querySelector('.ham-menu');
-    const offscreenMenu = document.querySelector('.off-screen-menu');
-  
-    if (hamMenu && offscreenMenu) {
-        hamMenu.addEventListener('click', () => {
-            hamMenu.classList.toggle('active');
-            offscreenMenu.classList.toggle('active');
-        });
-    } else {
-        
-        console.error('Ham menu or off-screen menu not found.');
+  const hamMenu = document.querySelector('.ham-menu');
+  const offscreenMenu = document.querySelector('.off-screen-menu');
+  if (hamMenu && offscreenMenu) {
+    hamMenu.addEventListener('click', () => {
+        hamMenu.classList.toggle('active');
+        offscreenMenu.classList.toggle('active');
+    });
+}
+  else {
+    console.error('Ham menu or off-screen menu not found.');
     }
 });
   
